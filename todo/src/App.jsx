7e42dffd1a921296ccb5a3683d56ac8e6ccbf6c6
Ayan <ayanbaryshev02@gmail.com>
2026-01-23 11:28:40 +0500

    add main html structure

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -6,7 +6,39 @@ function App() {
 
   return (
     <>
+     <header>
+      <h1>Simple To Do List</h1>
+      <p>Today is awesome day. The weather is awesome, you are awesome too!</p>
+     </header>
      
+     <main>
+
+      <div className='pager'>
+      <button>To Do</button>
+       <button>Done</button>
+        <button>Trash</button>
+        <button>+</button>
+     </div>
+
+      <div className='sectionName'>
+        <h2>To Do</h2>
+      </div>
+
+      <div className='itemList'>
+      <ul>
+        <li>Write Essay</li>
+         <li>One Hour CSS Course Online</li>
+          <li>Go to Gym</li>
+      </ul>
+      </div>
+
+     </main>
+     <footer>
+
+    <p>Made with ❤️ at nFactorial in 2022.</p>
+    <p><a href='#'/>Credits: icons from Icons8.</p>
+
+     </footer>
     </>
   )
 }
