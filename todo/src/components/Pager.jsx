7c88508c57acfdf33c39079b9bfc821f86dc820d
Ayan <ayanbaryshev02@gmail.com>
2026-01-26 16:09:09 +0500

    added active tab for button (trash)

--- todo/src/components/Pager.jsx
+++ todo/src/components/Pager.jsx
@@ -4,9 +4,20 @@ const Pager = ({isModalOpen, setIsModalOpen, addNewTask, newTaskTitle, setNewTas
     return(
         <div className='flex justify-between mt-15 '>
         <div className=''>
-       <button onClick={()=> setActiveTab('ToDo')} className= {activeTab === "ToDo" ? 'ml-8 bg-gray-400 rounded-full p-2 px-6 font-medium text-white' :'ml-8 bg-gray-200 rounded-full p-2 px-6 font-medium text-black'}>To Do</button>
-       <button onClick={()=> setActiveTab('Done')} className= {activeTab === "Done" ? 'ml-8 bg-gray-400 rounded-full p-2 px-6 font-medium text-white' :'ml-8 bg-gray-200 rounded-full p-2 px-6 font-medium text-black'}>Done</button>
-        <button className='ml-8 bg-gray-200  rounded-full p-2 px-6 font-medium'>Trash</button>
+       <button onClick={()=> setActiveTab('ToDo')} 
+       className= {activeTab === "ToDo" 
+       ? 'ml-8 bg-gray-400 rounded-full p-2 px-6 font-medium text-white' 
+       : 'ml-8 bg-gray-200 rounded-full p-2 px-6 font-medium text-black'}>To Do</button>
+
+       <button onClick={()=> setActiveTab('Done')} 
+       className= {activeTab === "Done" 
+       ? 'ml-8 bg-gray-400 rounded-full p-2 px-6 font-medium text-white' 
+       : 'ml-8 bg-gray-200 rounded-full p-2 px-6 font-medium text-black'}>Done</button>
+
+        <button onClick={()=> setActiveTab('Trash')} 
+        className= {activeTab === "Trash" 
+        ? 'ml-8 bg-gray-400 rounded-full p-2 px-6 font-medium text-white' 
+        :'ml-8 bg-gray-200 rounded-full p-2 px-6 font-medium text-black'}>Trash</button>
         </div>
         <AddTaskButton isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
